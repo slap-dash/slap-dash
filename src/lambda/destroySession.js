deb8f8b857b9166a8902533284d55aa6c75ef271
Jake Shasteen <jakeshasteen@gmail.com>
2018-03-24 16:45:10 -0700

    Add lambdas (#11)
    * Add placeholder lambda functions
    
    * Remove hello
    
    * Create sessions with TokBox

---
+++ src/lambda/destroySession.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+export function handler(event, context, callback) {
+  const API_KEY = process.env.TOKBOX_API_KEY;
+  const SECRET = process.env.TOKBOX_SECRET;
+
+  // Talk to tokbox API
+  // Destroy session
+  // Remove from DB
+  
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({msg: API_KEY})
+  })
+}
