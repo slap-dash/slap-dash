d91e3f8afd4b14606b0fd4baacdabcb05b57a5a1
Jake Shasteen <jakeshasteen@gmail.com>
2018-03-24 15:50:01 -0700

    Add placeholder lambda functions

--- src/lambda/destroySession.js
+++ src/lambda/destroySession.js
@@ -4,9 +4,10 @@ export function handler(event, context, callback) {
   const API_KEY = process.env.TOKBOX_API_KEY;
   const SECRET = process.env.TOKBOX_SECRET;
 
-  // Hit tokbox API to create session
-  // store session information in db
-
+  // Talk to tokbox API
+  // Destroy session
+  // Remove from DB
+  
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({msg: API_KEY})
--- src/lambda/listSessions.js
+++ src/lambda/listSessions.js
@@ -4,9 +4,9 @@ export function handler(event, context, callback) {
   const API_KEY = process.env.TOKBOX_API_KEY;
   const SECRET = process.env.TOKBOX_SECRET;
 
-  // Hit tokbox API to create session
-  // store session information in db
-
+  // Hit DB
+  // List all sessions
+  
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({msg: API_KEY})
