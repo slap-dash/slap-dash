000680b95cb501b4f0a5ea059076ee19c42132fd
SeungJu Lee <me@sjlee.me>
2018-03-24 15:39:13 -0700

    Update styles with responsive grid

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import TokBoxDemo from './TokBoxDemo.js';
 
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
+    const App = styled.div`
+      text-align: center;
+      background-color: #222;
+      height: 100vh;
+      width: 100vw;
+      color: white;
+    `;
+
     return (
       <App>
         <TokBoxDemo />
