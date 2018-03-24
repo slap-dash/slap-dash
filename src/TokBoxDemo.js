263afc2f8ca9502cbf395d9f44897788937d3b8f
Jake Shasteen <jakeshasteen@gmail.com>
2018-03-24 14:31:40 -0700

    Add keys for testing (#4)
    * Add keys for testing
    
    * Use sessionHelper

--- src/TokBoxDemo.js
+++ src/TokBoxDemo.js
@@ -4,18 +4,44 @@ import {
   OTSession,
   OTPublisher,
   OTStreams,
-  OTSubscriber
+  OTSubscriber,
+  createSession
 } from 'opentok-react';
 
 class TokBoxDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { streams: [] };
+  }
+ 
+  componentWillMount() {
+    this.sessionHelper = createSession({
+      apiKey: '46086782',
+      sessionId: '1_MX40NjA4Njc4Mn5-MTUyMTkyNjI5NTA4N35RWHEwNVA5NEJDY0VwL2lEWGdSMTNGZm5-UH4',
+      token: 'T1==cGFydG5lcl9pZD00NjA4Njc4MiZzaWc9OWFlZTkyYjdhNDIxODIwOTY3ZWJjY2RiNmM2MmEyNjUzYjlkZmE0NTpzZXNzaW9uX2lkPTFfTVg0ME5qQTROamM0TW41LU1UVXlNVGt5TmpJNU5UQTROMzVSV0hFd05WQTVORUpEWTBWd0wybEVXR2RTTVROR1ptNS1VSDQmY3JlYXRlX3RpbWU9MTUyMTkyNjM0MyZub25jZT0wLjg4OTQ4NjY2NDQ2NjEyNTImcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUyMjUzMTE0MyZjb25uZWN0aW9uX2RhdGE9cHVibGlzaGVyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
+      onStreamsUpdated: streams => { this.setState({ streams }); }
+    });
+  }
+ 
+  componentWillUnmount() {
+    this.sessionHelper.disconnect();
+  }
+ 
   render() {
     return (
-      <OTSession apiKey="your-api-key" sessionId="your-session-id" token="your-session-token">
-        <OTPublisher />
-        <OTStreams>
-          <OTSubscriber />
-        </OTStreams>
-      </OTSession>
+      <div>
+        <OTPublisher session={this.sessionHelper.session} />
+ 
+        {this.state.streams.map(stream => {
+          return (
+            <OTSubscriber
+              key={stream.id}
+              session={this.sessionHelper.session}
+              stream={stream}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
