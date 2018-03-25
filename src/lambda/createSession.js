2e40ac9b95593076e37f21341f02d1c6389199ab
SeungJu Lee <me@sjlee.me>
2018-03-24 18:29:03 -0700

    Update createSession

--- src/lambda/createSession.js
+++ src/lambda/createSession.js
@@ -1,37 +1,33 @@
-require('dotenv').config()
-const axios = require('axios');
-const jwt = require('json-web-token');
-const uuidv4 = require('uuid/v4');
+require('dotenv').config();
+const OpenTok = require('opentok');
 
-const nowInSeconds = () => Math.floor(Date.now() / 1000)
-
+// eslint-disable-next-line
 export function handler(event, context, callback) {
   const API_KEY = process.env.TOKBOX_API_KEY;
   const SECRET = process.env.TOKBOX_SECRET;
-  const tokbox_url = 'https://api.opentok.com/session/create';
+  const opentok = new OpenTok(API_KEY, SECRET);
 
-  // Hit tokbox API to create session
-  // store session information in db
+  // eslint-disable-next-line
+  opentok.createSession((err, session) => {
+    if (err) return console.log(err);
 
-  const tokbox_auth_payload = jwt.encode(SECRET, {
-    'iss': API_KEY,
-    'ist': 'project',
-    'iat': nowInSeconds(),
-    'exp': nowInSeconds() + 180,
-    'jti': uuidv4(),
-  });
+    const publisherToken = opentok.generateToken(session.sessionId, { role: 'publisher' });
+    const subscriberToken = opentok.generateToken(session.sessionId, { role: 'subscriber' });
 
-  axios.post(tokbox_url, '', {
-    headers: {
-      'X-OPENTOK-AUTH': tokbox_auth_payload.value,
-      'accept': 'application/json',
-    },
-  })
-  .then(json => {
+    // save the sessionId
+    // db.save('session', session.sessionId, done);
+    const response = {
+      roomName: 'hello_world',
+      sessionId: session.sessionId,
+      token: {
+        publisher: publisherToken,
+        subscriber: subscriberToken,
+      },
+    };
+
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ msg: json.data })
-    })
-  })
-  .catch(exception => callback(exception));
+      body: JSON.stringify(response),
+    });
+  });
 }
