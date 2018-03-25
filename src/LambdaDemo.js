df6dc2c54c265cf2dc7cda54f975bdeeaae0affa
Haydenmarx <haydenmarx@gmail.com>
2018-03-25 10:12:31 -0700

    Updating to more recent versions

---
+++ src/LambdaDemo.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, msg: null};
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+
+    this.setState({loading: true});
+    fetch('/.netlify/functions/createSession')
+      .then(response => response.json())
+      .then(json => this.setState({loading: false, msg: json.msg}));
+  }
+
+  render() {
+    const {loading, msg} = this.state;
+
+    return <p>
+      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
+      <span>{JSON.stringify(msg)}</span>
+    </p>
+  }
+}
---
+++ src/lambda/createSession.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const OpenTok = require('opentok');
+
+// eslint-disable-next-line
+export function createSession(event, context, callback) {
+  const API_KEY = process.env.TOKBOX_API_KEY;
+  const SECRET = process.env.TOKBOX_SECRET;
+  const opentok = new OpenTok(API_KEY, SECRET);
+
+  const inputPayload = JSON.parse(event.body);
+
+  // eslint-disable-next-line
+  opentok.createSession((err, session) => {
+    if (err) return callback(err);
+
+    const publisherToken = opentok.generateToken(session.sessionId, { role: 'publisher' });
+    const subscriberToken = opentok.generateToken(session.sessionId, { role: 'subscriber' });
+
+    const response = {
+      room_name: inputPayload.room_name,
+      session_id: session.sessionId,
+      token: {
+        publisher: publisherToken,
+        subscriber: subscriberToken,
+      },
+    };
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    });
+  });
+}
