b1af30c64dfe433664f7c0bdc60e16e9a614557d
SeungJu Lee <me@sjlee.me>
2018-03-24 18:46:10 -0700

    Restify backend sessions (#19)
    * Update createSession
    
    * Remove unnecessary packages
    
    * Revert the default token to subscriber token

--- src/lambda/listSessions.js
+++ src/lambda/listSessions.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
 
-export function handler(event, context, callback) {
+// eslint-disable-next-line
+export function listSessions(event, context, callback) {
   const API_KEY = process.env.TOKBOX_API_KEY;
   const SECRET = process.env.TOKBOX_SECRET;
 
   // Hit DB
   // List all sessions
-  
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: API_KEY})
-  })
+    body: JSON.stringify({ msg: API_KEY }),
+  });
 }
