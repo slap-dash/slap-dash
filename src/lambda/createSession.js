3aeb87e5dccff06db4dbe6223e64165c710ddc5b
SeungJu Lee <me@sjlee.me>
2018-03-24 18:57:25 -0700

    Resole conflicts

--- src/lambda/createSession.js
+++ src/lambda/createSession.js
@@ -7,7 +7,6 @@ export function createSession(event, context, callback) {
   const SECRET = process.env.TOKBOX_SECRET;
   const opentok = new OpenTok(API_KEY, SECRET);
 
-<<<<<<< HEAD
   const inputPayload = JSON.parse(event.body);
 
   // eslint-disable-next-line
@@ -20,20 +19,6 @@ export function createSession(event, context, callback) {
     const response = {
       room_name: inputPayload.room_name,
       session_id: session.sessionId,
-=======
-  // eslint-disable-next-line
-  opentok.createSession((err, session) => {
-    if (err) return callback(err);
-
-    const publisherToken = opentok.generateToken(session.sessionId, { role: 'publisher' });
-    const subscriberToken = opentok.generateToken(session.sessionId, { role: 'subscriber' });
-
-    // save the sessionId
-    // db.save('session', session.sessionId, done);
-    const response = {
-      roomName: 'hello_world',
-      sessionId: session.sessionId,
->>>>>>> master
       token: {
         publisher: publisherToken,
         subscriber: subscriberToken,
